4eb3d1295f2c2d15d17c0723080a88011d983ba2
Armin Sander <armin@replicator.org>
2013-08-04 19:28:43 +0200

    add some more tests

---
+++ Instant.Tests/EdgeCaseTests.fs
@@ -0,0 +1,33 @@
+﻿namespace Instant.Tests.EdgeCaseTests
+
+open NUnit.Framework
+open FsUnit
+
+open Instant
+
+[<TestFixture>]
+type EdgeCaseTests() = class
+
+    // Tratt 
+    // http://tratt.net/laurie/research/pubs/papers/tratt__direct_left_recursive_parsing_expression_grammars.pdf
+    // 5. Incorrect Parses with the Origin LR algorithm from Warth et al.
+
+    
+    let tratt51Grammar = 
+        let expr = ref None
+        let num = oneOf "0123456789"
+
+        expr := !!expr +. ~~"-" +. !!expr +* fun ((a, _), c) -> "(" + a + "-" + c + ")"
+            |. num +* fun c -> string(c)
+            |> Some
+
+        (!expr).Value
+
+
+    // this is actually wrong, in this special case, the production above is handled right-associative
+    // according to Tratt, the result should be ((1-2)-3)
+    [<Test>]
+    member this.tratt51() =
+        (parse tratt51Grammar "1-2-3").Value |> should equal "(1-(2-3))"
+
+end
---
+++ Instant.Tests/PrimitiveTests.fs
@@ -0,0 +1,97 @@
+﻿namespace Instant.Tests
+
+open NUnit.Framework
+open FsUnit
+
+open Instant
+open TestGrammars
+
+[<TestFixture>]
+type PrimitiveTests() = class
+
+    let orGrammar = ~~"Hello" |. ~~"World"
+    let andGrammar = ~~"Hello" +. ~~"World"
+
+
+    let helloOrWorldBuilder = 
+        ~~"Hello" 
+        |. 
+        ~~"World" +. ~~"Builder" +* fun (a,b) -> (a + b)
+
+    let lrGrammar =
+        let hello = ~~"Hello"
+
+        let g = ref None
+        g := !!g +. hello +* fun (a, b) -> (a + b)
+             |.
+             hello
+             |> Some
+
+        (!g).Value
+
+    [<Test>]
+    member this.parseOr1() =
+        let r = parse orGrammar "Hello"
+        r.Value |> should equal "Hello"
+
+    [<Test>]
+    member this.parseOr2() =
+        let r = parse orGrammar "World"
+        r.Value |> should equal "World"
+
+    [<Test>]
+    member this.parseOrF() =
+        let r = parse orGrammar "W"
+        r.value |> should equal None
+
+    [<Test>]
+    member this.parseLess() =
+        let r = parse orGrammar "HelloWorld"
+        r.Value |> should equal "Hello"
+
+    [<Test>]
+    member this.ParseAnd2() =
+        let r = parse andGrammar "HelloWorld"
+        r.Value |> should equal ("Hello", "World")
+
+    [<Test>]
+    member this.ParsePrecendence1() =
+        let p = parse helloOrWorldBuilder "Hello"
+        p.Value |> should equal "Hello"
+
+    [<Test>]
+    member this.ParsePrecendence2() =
+        let p = parse helloOrWorldBuilder "WorldBuilder"
+        p.Value |> should equal "WorldBuilder"
+
+    [<Test>]
+    member this.ParsePrecendence3() =
+        let p = parse helloOrWorldBuilder "HelloWorldBuilder"
+        p.Value |> should equal "Hello"
+
+    [<Test>]
+    member this.ParsePrecendence4() =
+        let p = parse helloOrWorldBuilder "HelloWorld"
+        p.Value |> should equal "Hello"
+
+    [<Test>]
+    member this.ParseImmediateLR() =
+        let r = parse lrGrammar "HelloHello"
+        r.Value |> should equal "HelloHello"
+
+    [<Test>]
+    member this.ParseImmediateLR1() =
+        let r = parse lrGrammar "Hello"
+        r.Value |> should equal "Hello"
+
+    [<Test>]
+    member this.ParseImmediateLR3() =
+        let r = parse lrGrammar "HelloHelloHello"
+        r.Value |> should equal "HelloHelloHello"
+
+    [<Test>]
+    member this.ParseDigits1() =
+        let r = parse digits "123"
+        r.Value |> should equal 123
+
+end
