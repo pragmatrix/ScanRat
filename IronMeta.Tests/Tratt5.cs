de2d44df595ad1bdf5af9fb121201f32eb6e8ded
Armin Sander <armin@replicator.org>
2013-08-13 20:20:59 +0200

    add IronMeta.Tests, verify the tratt5 error with IronMeta, and enable automatic package restore

---
+++ IronMeta.Tests/Tratt5.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace IronMeta.Tests
+{
+	[TestFixture]
+	public class Tratt5
+	{
+		[Test]
+		public void testTratt5()
+		{
+			var c = new Calc();
+			var match = c.GetMatch("1-2-3", c.Expression);
+			Assert.True(match.Success);
+			Assert.That(match.Result, Is.EqualTo("(1-(2-3))"));
+		}
+	}
+}
